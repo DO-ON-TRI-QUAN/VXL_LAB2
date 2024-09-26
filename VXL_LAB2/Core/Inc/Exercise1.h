bcfba7976938daeba8f41135a175bda9d4a1495f
DO ON TRI QUAN <quan.do7neitiznot@hcmut.edu.vn>
2024-09-26 19:26:54 +0700

    no message

--- VXL_LAB2/Core/Inc/Exercise1.h
+++ VXL_LAB2/Core/Inc/Exercise1.h
@@ -5,9 +5,12 @@
  *
  */
 
+#include "main.h"
+
 #ifndef INC_EXERCISE1_H_
 #define INC_EXERCISE1_H_
 
-
+int status;
+void twoLEDBlinky();
 
 #endif /* INC_EXERCISE1_H_ */
--- VXL_LAB2/Core/Src/Exercise1.c
+++ VXL_LAB2/Core/Src/Exercise1.c
@@ -5,4 +5,5 @@
  *
  */
 
+#include "Exercise1.h"
 
