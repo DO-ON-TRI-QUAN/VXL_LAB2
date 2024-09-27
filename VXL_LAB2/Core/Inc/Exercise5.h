ca4cd5812d1743c8f1a5d9fa4a45805d0baa8092
DO ON TRI QUAN <quan.do7neitiznot@hcmut.edu.vn>
2024-09-27 16:12:36 +0700

    no message

---
+++ VXL_LAB2/Core/Inc/Exercise5.h
@@ -0,0 +1,19 @@
+/*
+ * Exercise5.h
+ *
+ *
+ *
+ */
+
+#include "main.h"
+#include "Exercise3.h"
+
+#ifndef INC_EXERCISE5_H_
+#define INC_EXERCISE5_H_
+
+int hour;
+int minute;
+int second;
+void updateClockBuffer();
+
+#endif /* INC_EXERCISE5_H_ */
---
+++ VXL_LAB2/Core/Src/Exercise5.c
@@ -0,0 +1,24 @@
+/*
+ * Exercise5.c
+ *
+ *
+ *
+ */
+
+#include "Exercise5.h"
+
+int hour = 15;
+int minute = 9;
+int second = 50;
+
+void updateClockBuffer() {
+
+	/*
+	LED_buffer[0] = hour / 10;
+	LED_buffer[1] = hour % 10;
+	LED_buffer[2] = minute / 10;
+	LED_buffer[3] = minute % 10;
+    */
+
+	update7SEG(index_LED++);
+}
