d646af951ec913e190bcb290bb453d8aaa7d0eec
DO ON TRI QUAN <quan.do7neitiznot@hcmut.edu.vn>
2024-09-29 16:08:39 +0700

    no message

--- VXL_LAB2/Core/Src/Exercise5.c
+++ VXL_LAB2/Core/Src/Exercise5.c
@@ -8,17 +8,14 @@
 #include "Exercise5.h"
 
 int hour = 15;
-int minute = 9;
+int minute = 8;
 int second = 50;
 
 void updateClockBuffer() {
-
-	/*
 	LED_buffer[0] = hour / 10;
 	LED_buffer[1] = hour % 10;
 	LED_buffer[2] = minute / 10;
 	LED_buffer[3] = minute % 10;
-    */
 
 	update7SEG(index_LED++);
 }
